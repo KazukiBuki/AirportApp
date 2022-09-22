f5bb39a87118812f9302fdd09e1747074c7a448e
Moksleivis <Moksleivis@kitm.local>
2022-09-22 11:17:09 +0300

    first commit

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg bg-success">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Air BnB</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Airports</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Countries</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Airlines</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body style="background-color: lightgreen;">
+    @include('partials/navbar')
+    
+    <div class="container">
+
+    
+
+        <div class="d-flex justify-content-center">
+        <a class="d-flex justify-content-center btn btn-danger" style="margin-top: 13px;">New airport</a>
+        </div>
+    
+      <div class="form-group nav d-flex justify-content-center col-sm-12" style="margin-top: 13px;">
+        <select class="form-control" id="country">
+          <option>Search by country</option>
+          <option>Lithuania</option>
+          <option>Scotland</option>
+          <option>United Kingdom</option>
+        </select>
+      </div>
+
+      <div class="d-flex justify-content-center">
+        <a class="d-flex justify-content-center btn btn-warning" style="margin-top: 13px;">Search</a>
+        </div>
+
+        <div class="table" style="margin-top: 13px;">
+        <table class="table table-bordered bg-warning">
+           <thead>
+             <tr>
+               <th scope="col">Name</th>
+               <th scope="col">Country</th>
+               <th scope="col">Location</th>
+               <th scope="col">Airlines</th>
+               <th scope="col">Actions</th>
+             </tr>
+           </thead>
+           <tbody>
+             <tr>
+               <th scope="row">Brexit</th>
+               <td>United Kingdom</td>
+               <td>Walter</td>
+               <td>Sample Text</td>
+               <td>
+                 <a class="btn btn-primary">Edit</a>
+                 <a class="btn btn-info">Add airline</a>
+                 <a class="btn btn-secondary">Remove airline</a>
+                 <a class="btn btn-danger">Delete</a>
+               </td>
+             </tr>
+           </tbody>
+        </table>
+      </div>
+</body>
+</html>
